8c5bb2141b1ac089f2b42106e5ce22ca6471cdd6
julienmalard <julien.malard@mail.mcgill.ca>
2024-02-02 15:04:02 +0100

    Sans les frousfrous

--- scripts/compiler-github.mjs
+++ scripts/compiler-github.mjs
@@ -1,27 +1,18 @@
 import { execa } from 'execa';
-import emoji from 'node-emoji'
-import chalk from 'chalk'
-
-const firstLog = `${emoji.get('fast_forward')} ${chalk.yellow('Building...')}`
-const secondLog = `${emoji.get('fast_forward')} ${chalk.yellow('Pushing...')}`
-const thirdLog = `${emoji.get('rocket')} ${chalk.green('Your app successfully deployed')} ${emoji.get('rocket')}`
 
 ;(async () => {
   try {
     const { stdout: currentBranch } = await execa('git', ['branch', '--show-current'])
     await execa('git', ['checkout', '--orphan', 'gh-pages'])
-    console.log(firstLog)
     await execa('pnpm', ['compiler'], { stdio: 'inherit' })
 
     await execa('git', ['--work-tree', 'dist', 'add', '--all'])
     await execa('git', ['--work-tree', 'dist', 'commit', '-m', '"gh-pages"'])
-    console.log(secondLog)
 
     await execa('git', ['push', 'origin', 'HEAD:gh-pages', '--force'], { stdio: 'inherit' })
     await execa('rm', ['-r', 'dist'])
     await execa('git', ['checkout', '-f', currentBranch])
     await execa('git', ['branch', '-D', 'gh-pages'])
-    console.log(thirdLog)
 
   } catch (e) {
     console.log(e.message)
