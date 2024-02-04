c0753f4e985d608b2c0f4dbab092e7066eb62823
julienmalard <julien.malard@mail.mcgill.ca>
2024-02-04 09:25:04 +0100

    Sans serveur express

--- src/serveur.ts
+++ src/serveur.ts
@@ -1,10 +1,13 @@
-import express from "express";
+/*import express from "express";
 import path from "path";
 import serveStatic from "serve-static";
-import compression from "compression";
+import compression from "compression";*/
 
 import { créerNœud, obtAdressesNœud } from "./relai.js";
 
+const nœud = await créerNœud();
+obtAdressesNœud(nœud);
+/*
 const app = express();
 
 app.use(compression());
@@ -14,8 +17,7 @@ app.use(serveStatic(path.join("/dist")));
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-const nœud = await créerNœud();
-obtAdressesNœud(nœud);
+
 app.get("/", (_request, response) => {
   response.sendFile(path.join("src", "index.html"), { root: "." });
 });
@@ -26,3 +28,4 @@ app.get("/adresses", async (_request, response) => {
 
 const port = process.env.PORT || 8000;
 app.listen(port);
+*/
